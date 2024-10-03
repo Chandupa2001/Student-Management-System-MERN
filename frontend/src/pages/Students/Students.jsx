1f781232598ea56328504e3fdf799b76227dc83a
Chandupa2001 <chanduparanawaka2001@gmail.com>
2024-10-03 11:01:22 +0530

    Conected fectch student backend with frontend

--- frontend/src/pages/Students/Students.jsx
+++ frontend/src/pages/Students/Students.jsx
@@ -2,18 +2,41 @@ import React, { useEffect, useState } from "react";
 import "./Students.css";
 import { FaRegEdit } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function Items() {
-
   const navigate = useNavigate();
 
+  const [students, setStudents] = useState([]);
+
+  useEffect(() => {
+    fetchStudents();
+  }, []);
+
+  const fetchStudents = async () => {
+    const url = "http://localhost:3000/api/student/get";
+    try {
+      const response = await axios.get(url);
+      console.log(response.data);
+
+      if (response.data.success) {
+        setStudents(response.data.data);
+      } else {
+        console.error("Error");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div>
       <div className="table-container">
         <div className="title-container">
-            <button onClick={() => navigate('/dashboard/add-student')}>Add Student</button>
-            <h2>List of Students</h2>
+          <button onClick={() => navigate("/dashboard/add-student")}>
+            Add Student
+          </button>
+          <h2>List of Students</h2>
         </div>
         <table className="items-table">
           <thead>
@@ -27,16 +50,20 @@ function Items() {
             </tr>
           </thead>
           <tbody>
-                <td>std1001</td>
-                <td>Chandupa Ranawaka</td>
-                <td>Matara</td>
-                <td>23</td>
-                <td>0711234567</td>
-                <td>
-                  <button>
-                    <FaRegEdit color="#6E726E" size={20} />
-                  </button>
-                </td>
+            {students.map((student) => (
+            <tr key={student.id}>
+              <td>{student.studentId}</td>
+              <td>{student.name}</td>
+              <td>{student.address}</td>
+              <td>{student.age}</td>
+              <td>{student.telephoneNo}</td>
+              <td>
+                <button>
+                  <FaRegEdit color="#6E726E" size={20} />
+                </button>
+              </td>
+            </tr>
+            ))}
           </tbody>
         </table>
       </div>
