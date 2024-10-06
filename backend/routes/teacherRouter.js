95b1f63eda3ca87e4137eeae6ab9d96ef12c864b
Chandupa2001 <chanduparanawaka2001@gmail.com>
2024-10-06 19:44:25 +0530

    Created fetch student backend

--- backend/routes/teacherRouter.js
+++ backend/routes/teacherRouter.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { addTeacher, fetchTeachers } from '../controllers/teacherController.js';
+import { addTeacher, fetchTeachers, viewTeacher } from '../controllers/teacherController.js';
 
 const teacherRouter = express.Router();
 
 teacherRouter.post("/add", addTeacher);
 teacherRouter.get("/get", fetchTeachers);
+teacherRouter.post("/view", viewTeacher);
 
 export default teacherRouter
