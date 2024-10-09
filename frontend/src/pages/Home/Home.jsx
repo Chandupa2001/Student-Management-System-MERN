d57bcf23be84c0dce343e581e69616d19ee5a977
Chandupa2001 <chanduparanawaka2001@gmail.com>
2024-10-10 00:33:17 +0530

    connected home page with backend

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -8,6 +8,9 @@ import axios from "axios";
 function Home() {
   const [token, setToken] = useState("");
   const [name, setName] = useState("");
+  const [students, setStudents] = useState("");
+  const [teachers, setTeachers] = useState('');
+  const [batches, setBatches] = useState('');
 
   useEffect(() => {
     const getToken = async () => {
@@ -16,6 +19,9 @@ function Home() {
     };
 
     getToken();
+    getStudents();
+    getTeachers();
+    getBatches();
   }, []);
 
   useEffect(() => {
@@ -36,6 +42,51 @@ function Home() {
     }
   };
 
+  const getStudents = async () => {
+    const url="http://localhost:3000/api/student/get";
+    try {
+      const response = await axios.get(url);
+      const students = response.data.data;
+      if (Array.isArray(students)) {
+        setStudents(students.length)
+      } else {
+        console.log('students is not an array');
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const getTeachers = async () => {
+    const url="http://localhost:3000/api/teacher/get";
+    try {
+      const response = await axios.get(url);
+      const teachers = response.data.data;
+      if (Array.isArray(teachers)) {
+        setTeachers(teachers.length)
+      } else {
+        console.log('Teacher is not an array');
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const getBatches = async () => {
+    const url="http://localhost:3000/api/batch/get";
+    try {
+      const response = await axios.get(url);
+      const batches = response.data.data;
+      if (Array.isArray(batches)) {
+        setBatches(batches.length)
+      } else {
+        console.log('class is not an array');
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
     <div className="container-home">
       <div>
@@ -46,21 +97,21 @@ function Home() {
           <p className="box-title">Students</p>
           <div className="box-items">
             <FaRegUser className="box-icon" size={25} />
-            <span className="box-text">10</span>
+            <span className="box-text">{students}</span>
           </div>
         </div>
         <div className="container-box">
           <p className="box-title">Teachers</p>
           <div className="box-items">
             <BsBoxSeam className="box-icon" size={25} />
-            <span className="box-text">10</span>
+            <span className="box-text">{teachers}</span>
           </div>
         </div>
         <div className="container-box">
-          <p className="box-title">Classes</p>
+          <p className="box-title">Batches</p>
           <div className="box-items">
             <PiListChecksBold className="box-icon" size={25} />
-            <span className="box-text">10</span>
+            <span className="box-text">{batches}</span>
           </div>
         </div>
       </div>
